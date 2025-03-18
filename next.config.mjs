c3207b75a03d7cc997e1dd800f19afc3dc1353a9
Mark Joshua Austria <austriamark.mja@gmail.com>
2025-03-18 14:56:42 +0800

    Refactor layout and improve accessibility; update Footer and FloatingNav components, and adjust Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,13 @@
 import { withSentryConfig } from "@sentry/nextjs";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // eslint: {
-  //   ignoreDuringBuilds: true,
-  // },
-  // typescript: {
-  //   ignoreBuildErrors: true,
-  // },
+  output: "export",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default withSentryConfig(nextConfig, {

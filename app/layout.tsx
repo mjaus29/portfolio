a521e84ebd8ec61207637e131981462c4593dc02
Mark Joshua Austria <austriamark.mja@gmail.com>
2025-04-22 15:58:55 +0800

    Update site title in metadata configuration
    Simplified the site title in the metadata from "Mark Austria's Portfolio" to "Mark Austria" for a cleaner presentation. This aligns the title with a more minimalist branding approach.

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Mark Austria's Portfolio",
+  title: "Mark Austria",
   description: "Modern & Minimalist Portfolio",
   icons: "/favicon.png",
 };

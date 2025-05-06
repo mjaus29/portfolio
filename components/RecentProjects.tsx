44501df42ac27dff0ecb32fbec0e962126af142f
Mark Joshua Austria <austriamark.mja@gmail.com>
2025-05-06 10:18:25 +0800

    Refactor projects data and improve project carousel UI
    Revised the projects dataset with new entries and updated links. Enhanced the "Recent Projects" component by introducing a carousel for better presentation, utilizing chunking for dynamic grouping, and integrating smoother animations. Added a new "PinPerspective" component for interactive hover effects.

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -3,65 +3,112 @@
 import { projects } from "@/data";
 import { PinContainer } from "./ui/3d-pin";
 import { FaLocationArrow } from "react-icons/fa6";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
+import Link from "next/link";
+
+const chunkArray = <T,>(arr: T[], size: number): T[][] =>
+  Array.from({ length: Math.ceil(arr.length / size) }, (_, i) =>
+    arr.slice(i * size, i * size + size),
+  );
 
 const RecentProjects = () => {
+  const chunks = chunkArray(projects, 4);
+
   return (
     <div className="py-20" id="projects">
       <h1 className="heading">
-        A small selection of{" "}
-        <span className="text-purple">recent projects</span>
+        A selection of <span className="text-purple">recent projects</span>
       </h1>
 
-      <div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10">
-        {projects.map(({ id, title, des, img, iconLists, link }) => (
-          <div
-            key={id}
-            className="sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]"
-          >
-            <PinContainer title={link} href={link}>
-              <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] mb-10">
-                <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162D]">
-                  <img src="/bg.png" alt="bg-img" />
-                </div>
-
-                <img src={img} alt={title} className="z-10 absolute bottom-0" />
-              </div>
-
-              <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
-                {title}
-              </h1>
-
-              <p className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2">
-                {des}
-              </p>
-
-              <div className="flex items-center justify-between mt-7 mb3">
-                <div className="flex items-center">
-                  {iconLists.map((icon, index) => (
+      <Carousel
+        className="mt-10"
+        plugins={[
+          Autoplay({
+            delay: 20000,
+          }),
+        ]}
+      >
+        <CarouselContent>
+          {chunks.map((group, i) => (
+            <CarouselItem key={i}>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-x-24 gap-y-8 p-4">
+                {group.map(
+                  ({ id, title, des, img, iconLists, link, project }) => (
                     <div
-                      key={icon}
-                      className="border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
-                      style={{ transform: `translateX(-${5 * index * 2}px)` }}
+                      key={id}
+                      className="sm:h-[41rem] h-[32rem] flex items-center justify-center sm:w-[570px] w-[80vw]"
                     >
-                      <img src={icon} alt={icon} className="p-2" />
+                      <PinContainer title={link} href={link}>
+                        <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] mb-10">
+                          <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162D]">
+                            <img src="/bg.png" alt="bg-img" />
+                          </div>
+
+                          <img
+                            src={img}
+                            alt={title}
+                            className="z-10 absolute bottom-[-10px] w-[90%] transform rotate-[2.63deg]"
+                          />
+                        </div>
+
+                        <Link href={`/${project}`} target="_blank">
+                          <h1 className="font-bold lg:text-xl md:text-xl text-base line-clamp-1">
+                            {title}
+                          </h1>
+
+                          <p className="lg:text-sm lg:font-normal font-light text-sm line-clamp-3">
+                            {des}
+                          </p>
+
+                          <div className="flex items-center justify-between mt-7 mb-3">
+                            <div className="flex items-center">
+                              {iconLists.map((icon, index) => (
+                                <div
+                                  key={icon}
+                                  className="border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
+                                  style={{
+                                    transform: `translateX(-${5 * index * 2}px)`,
+                                  }}
+                                >
+                                  <img src={icon} alt={icon} className="p-2" />
+                                </div>
+                              ))}
+                            </div>
+
+                            <div className="flex justify-center items-center cursor-pointer">
+                              <p className="flex lg:text-lg md:text-xs text-sm text-purple">
+                                Read more
+                              </p>
+                              <FaLocationArrow
+                                className="ms-3"
+                                color="#CBACF9"
+                              />
+                            </div>
+                          </div>
+                        </Link>
+                      </PinContainer>
                     </div>
-                  ))}
-                </div>
-
-                <div className="flex justify-center items-center">
-                  <p className="flex lg:text-xl md:text-xs text-sm text-purple">
-                    Check Live Site
-                  </p>
-                  <FaLocationArrow className="ms-3" color="#CBACF9" />
-                </div>
+                  ),
+                )}
               </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
 
-              <div></div>
-            </PinContainer>
-          </div>
-        ))}
-      </div>
+        <div className="flex justify-center gap-4 mt-6">
+          <CarouselPrevious />
+          <CarouselNext />
+        </div>
+      </Carousel>
     </div>
   );
 };
+
 export default RecentProjects;

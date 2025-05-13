2ed3e7f38333af7e309a364c68fc127fece21649
Mark Joshua Austria <austriamark.mja@gmail.com>
2025-05-13 11:29:34 +0800

    Add GitHub link and icons to DeployedButton component
    Enhanced the DeployedButton to include a link to the project's GitHub repository alongside the deployed link. Added icons for both links to improve visual clarity. Updated styles and tooltip functionality to maintain consistency.

--- components/DeployedButton.tsx
+++ components/DeployedButton.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 const DeployedButton = ({
   project,
@@ -15,25 +16,51 @@ const DeployedButton = ({
   return (
     <div className="inline-flex justify-center items-center gap-7 mb-16">
       {project !== "appraisal" && project !== "inspector" && (
-        <Link href={link} target="_blank" className="group relative">
-          <div className="flex-1 px-10 py-5 bg-gradient-to-r from-slate-900 to-slate-950 rounded-md outline outline-1 outline-offset-[-1px] outline-slate-500/40 flex justify-center items-center gap-2.5">
-            <div className="justify-center text-white text-lg font-medium leading-snug">
-              Deployed Link
+        <>
+          <Link href={link} target="_blank" className="group relative">
+            <div className="flex-1 px-10 py-5 bg-gradient-to-r from-slate-900 to-slate-950 rounded-md outline outline-1 outline-offset-[-1px] outline-slate-500/40 flex justify-center items-center gap-2.5">
+              <div className="w-4 h-4 relative overflow-hidden">
+                <Image
+                  src="/arrow.svg"
+                  className="w-4 h-4 left-0 top-0 absolute"
+                  alt="Arrow icon"
+                  width={16}
+                  height={16}
+                />
+              </div>
+              <div className="justify-center text-white text-lg font-medium leading-snug">
+                Live Site
+              </div>
             </div>
-          </div>
-          <span className="absolute left-1/2 -translate-x-1/2 -bottom-10 px-3 py-2 bg-slate-800 text-white text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity">
-            {tooltip}
-          </span>
-        </Link>
+            <span className="absolute left-1/2 -translate-x-1/2 -bottom-10 px-3 py-2 bg-slate-800 text-white text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity">
+              {tooltip}
+            </span>
+          </Link>
+          <Link
+            href={`https://github.com/mjaus29/${project}/`}
+            target="_blank"
+            className="group relative"
+          >
+            <div className="flex-1 px-10 py-5 bg-gradient-to-r from-slate-900 to-slate-950 rounded-md outline outline-1 outline-offset-[-1px] outline-slate-500/40 flex justify-center items-center gap-2.5">
+              <div className="w-4 h-4 relative overflow-hidden">
+                <Image
+                  src="/gh.png"
+                  className="w-4 h-4 left-0 top-0 absolute"
+                  alt="GitHub icon"
+                  width={16}
+                  height={16}
+                />
+              </div>
+              <div className="justify-center text-white text-lg font-medium leading-snug">
+                GitHub Repository
+              </div>
+            </div>
+            <span className="absolute left-1/2 -translate-x-1/2 -bottom-10 px-3 py-2 bg-slate-800 text-white text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity">
+              {`github.com/mjaus29/${project}`}
+            </span>
+          </Link>
+        </>
       )}
-      {/*<div className="flex-1 px-10 py-5 bg-gradient-to-r from-slate-900 to-slate-950 rounded-md outline outline-1 outline-offset-[-1px] outline-slate-500/40 flex justify-center items-center gap-2.5">*/}
-      {/*  <div className="w-4 h-4 relative overflow-hidden">*/}
-      {/*    <div className="w-4 h-4 left-0 top-0 absolute bg-white"></div>*/}
-      {/*  </div>*/}
-      {/*  <div className="justify-center text-white text-lg font-medium leading-snug">*/}
-      {/*    GitHub Repository*/}
-      {/*  </div>*/}
-      {/*</div>*/}
     </div>
   );
 };

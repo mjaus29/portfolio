7ba2e7554624db81ac6765fb139dc016980385bf
Mark Joshua Austria <austriamark.mja@gmail.com>
2026-01-06 13:10:58 +0800

    Enhance project not found page with improved styling and navigation link

--- app/[project]/page.tsx
+++ app/[project]/page.tsx
@@ -4,6 +4,8 @@ import Footer from "@/components/Footer";
 import ProjectCase from "@/components/ProjectCase";
 import * as projects from "@/data/results";
 import Hero from "@/components/Hero";
+import React from "react";
+import { FaLocationArrow } from "react-icons/fa6";
 
 const Project = ({
   params,
@@ -21,12 +23,21 @@ const Project = ({
         <div className="max-w-7xl w-full flex flex-col items-center justify-center min-h-screen">
           <FloatingNav navItems={navItems} />
           <div className="max-w-full flex-grow flex flex-col items-center justify-center">
-            <h1 className="text-5xl font-bold text-white mb-4">
+            <h1 className="text-7xl font-bold text-white mb-4">
               Project Not Found
             </h1>
-            <p className="text-lg text-gray-400 mb-8">
+            <p className="text-3xl text-gray-400 mb-8">
               The project you are looking for does not exist.
             </p>
+            <div className="flex lg:text-lg md:text-xs text-sm text-purple cursor-pointer">
+              <a
+                href="https://markaustria.com"
+                rel="noopener noreferrer"
+                className="flex lg:text-lg md:text-xs text-sm text-purple cursor-pointer"
+              >
+                Go back to Home
+              </a>
+            </div>
           </div>
 
           <Footer />
